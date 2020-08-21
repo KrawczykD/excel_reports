2b4b49516f5c06f178e81b2afa7d27ed8cd10d41
Damian Krawczyk <damiankrawczyk23@gmail.com>
2020-08-21 10:01:35 +0100

    Updates

---
+++ precache-manifest.2ebff754b8c901b80c295e5c7a43a29e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e720da1e5139a42849572e2e12f37cbc",
+    "url": "/excel_reports/index.html"
+  },
+  {
+    "revision": "3788542053ecc8073b74",
+    "url": "/excel_reports/static/js/2.cfe34ab2.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/excel_reports/static/js/2.cfe34ab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a01359abd62a64b331f",
+    "url": "/excel_reports/static/js/main.543a9904.chunk.js"
+  },
+  {
+    "revision": "db8d4807816cd222f8be",
+    "url": "/excel_reports/static/js/runtime-main.e7bcc030.js"
+  }
+]);
